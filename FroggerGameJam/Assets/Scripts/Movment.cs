1f2146eab3fbc57b45c3da3ffb732fae67ccd7aa
Alex <adl.wolenetz@gmail.com>
2019-09-05 13:51:41 -0700

    Put Delay in movement

--- FroggerGameJam/Assets/Scripts/Movment.cs
+++ FroggerGameJam/Assets/Scripts/Movment.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
+    float MoveTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveTimer = .05f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        
+        MoveTimer -= Time.deltaTime;
+
+        print(MoveTimer);
+
+        if (Input.GetKeyDown(KeyCode.W) && MoveTimer <= 0)
         {
-            gameObject.transform.position += new Vector3(0,2,0);
+            gameObject.transform.position += new Vector3(0,1,0);
+            MoveTimer = .05f;
+        }
+        if (Input.GetKeyDown(KeyCode.A) && MoveTimer <= 0)
+        {
+            gameObject.transform.position += new Vector3(-1,0,0);
+            MoveTimer = .05f;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && MoveTimer <= 0)
+        {
+            gameObject.transform.position += new Vector3(1,0,0);
+            MoveTimer = .05f;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && MoveTimer <= 0)
+        {
+            gameObject.transform.position += new Vector3(0,-1,0);
+            MoveTimer = .05f;
         }
     }
 }
