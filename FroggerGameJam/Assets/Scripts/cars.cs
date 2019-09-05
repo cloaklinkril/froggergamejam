bd8d5cfc7a7b3e01efd13ee5adf4e40883032369
riley <whiskeydude360@gmail.com>
2019-09-05 15:12:32 -0700

    bruv

---
+++ FroggerGameJam/Assets/Scripts/cars.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cars : MonoBehaviour
+{
+    public float timer;
+    float timertemp;
+    public GameObject car;
+    public float x;
+    public float y;
+    public float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timertemp = timer;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == ("player"))
+        {
+            Destroy(car);
+            Debug.Log("lkdskdskjldsfjkljkdsfkjlfkljfkjlfdsjkl");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        transform.position += new Vector3(0.09f, 0, 0);
+        if(timer<=0)
+        {
+            transform.position = new Vector3(x, y, z);
+
+            timer = timertemp;   
+        }
+    }
+}
