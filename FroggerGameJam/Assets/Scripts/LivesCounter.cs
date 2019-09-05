66d5c057d793d7923bda4fe7964fc5610f5d9377
Alex <adl.wolenetz@gmail.com>
2019-09-05 15:03:01 -0700

    Added lives counter script

---
+++ FroggerGameJam/Assets/Scripts/LivesCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesCounter : MonoBehaviour
+{
+    public int Lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Lives <= 0)
+        {
+
+        }
+    }
+}
--- FroggerGameJam/Assets/Scripts/Movment.cs
+++ FroggerGameJam/Assets/Scripts/Movment.cs
@@ -6,10 +6,11 @@ public class Movment : MonoBehaviour
 {
     
     public float MoveTimer;
+    public float ResetTimer;
     // Start is called before the first frame update
     void Start()
     {
-        MoveTimer = .5f;
+        MoveTimer = ResetTimer;
     }
 
     // Update is called once per frame
@@ -23,22 +24,22 @@ public class Movment : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W) && MoveTimer <= 0)
         {
             gameObject.transform.position += new Vector3(0,1,0);
-            MoveTimer = .07f;
+            MoveTimer = ResetTimer;
         }
         if (Input.GetKeyDown(KeyCode.A) && MoveTimer <= 0)
         {
             gameObject.transform.position += new Vector3(-1,0,0);
-            MoveTimer = .07f;
+            MoveTimer = ResetTimer;
         }
         if (Input.GetKeyDown(KeyCode.D) && MoveTimer <= 0)
         {
             gameObject.transform.position += new Vector3(1,0,0);
-            MoveTimer = .07f;
+            MoveTimer = ResetTimer;
         }
         if (Input.GetKeyDown(KeyCode.S) && MoveTimer <= 0)
         {
             gameObject.transform.position += new Vector3(0,-1,0);
-            MoveTimer = .07f;
+            MoveTimer = ResetTimer;
         }
     }
 }
