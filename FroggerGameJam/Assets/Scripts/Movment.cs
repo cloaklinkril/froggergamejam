508f416931455deb89453f4ec49b86fd3bac9dba
cloaklinkril <whiskeydude360@gmail.com>
2019-09-10 11:22:59 -0700

    qwd

--- FroggerGameJam/Assets/Scripts/Movment.cs
+++ FroggerGameJam/Assets/Scripts/Movment.cs
@@ -19,27 +19,28 @@ public class Movment : MonoBehaviour
         
         MoveTimer -= Time.deltaTime;
 
-        print(MoveTimer);
+        //print(MoveTimer);
 
-        if (Input.GetKeyDown(KeyCode.W) && MoveTimer <= 0)
+        if (Input.GetKeyDown(KeyCode.W) /*&& MoveTimer <= 0*/)
         {
             gameObject.transform.position += new Vector3(0,1,0);
             MoveTimer = ResetTimer;
+            Debug.Log("brub");
         }
-        if (Input.GetKeyDown(KeyCode.A) && MoveTimer <= 0)
-        {
-            gameObject.transform.position += new Vector3(-1,0,0);
-            MoveTimer = ResetTimer;
-        }
-        if (Input.GetKeyDown(KeyCode.D) && MoveTimer <= 0)
-        {
-            gameObject.transform.position += new Vector3(1,0,0);
-            MoveTimer = ResetTimer;
-        }
-        if (Input.GetKeyDown(KeyCode.S) && MoveTimer <= 0)
-        {
-            gameObject.transform.position += new Vector3(0,-1,0);
-            MoveTimer = ResetTimer;
-        }
+       // if (Input.GetKeyDown(KeyCode.A) && MoveTimer <= 0)
+       // {
+       //     gameObject.transform.position += new Vector3(-1,0,0);
+       //     MoveTimer = ResetTimer;
+       // }
+       // if (Input.GetKeyDown(KeyCode.D) && MoveTimer <= 0)
+       // {
+       //     gameObject.transform.position += new Vector3(1,0,0);
+       //     MoveTimer = ResetTimer;
+       // }
+       // if (Input.GetKeyDown(KeyCode.S) && MoveTimer <= 0)
+       // {
+       //     gameObject.transform.position += new Vector3(0,-1,0);
+       //     MoveTimer = ResetTimer;
+       // }
     }
 }
