458739cffad674c7473efcc1d044e760025c221a
riley <whiskeydude360@gmail.com>
2019-09-17 11:48:17 -0700

    12345678910

--- FroggerGameJam/Assets/Scripts/water.cs
+++ FroggerGameJam/Assets/Scripts/water.cs
@@ -7,6 +7,7 @@ public class water : MonoBehaviour
     public GameObject player;
     public bool part1 = false;
     public bool part2 = false;
+
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -26,6 +27,16 @@ public class water : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var dissablecolliders = FindObjectOfType<log>().dissablecollider;
+        var tryeyy = FindObjectOfType<Movment>().tryey;
+        if(tryeyy)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false; 
+        }
+        if(tryeyy == false)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
         var logo = FindObjectOfType<log>();
         if(logo.checkpls == true)
         {
