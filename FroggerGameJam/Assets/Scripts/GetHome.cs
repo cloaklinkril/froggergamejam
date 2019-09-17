2311554dcffe9241e5ac8ea05ad9fa03b5332b55
KalebPoquette <kaleb.poquette@digipen.edu>
2019-09-17 11:51:41 -0700

    Goal

---
+++ FroggerGameJam/Assets/Scripts/GetHome.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Start is called before the first frame update
+    public bool frogHome = false; 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if(!frogHome)
+                frogHome = true;
+        }
+    }
+}
---
+++ FroggerGameJam/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WinCondition : MonoBehaviour
+{
+    public GameObject[] goals;
+    public UnityEvent GameWon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        bool everythingDone = true;
+        goals = GameObject.FindGameObjectsWithTag("Goal");
+        foreach(GameObject checkGoal in goals)
+        {
+            if (checkGoal.GetComponent<GetHome>().frogHome == false)
+            {
+                everythingDone = false;
+            }
+        }
+        if (everythingDone == true)
+        {
+            GameWon.Invoke();
+        }
+    }
+}
